da3ade858451924181473193cdcf65cced872763
Maks0m0s <maksym.kozlovskyi150210@gmail.com>
2026-01-31 18:27:37 +0100

    settings email update

--- config/settings/base.py
+++ config/settings/base.py
@@ -150,8 +150,8 @@ EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = int(os.environ.get("EMAIL_PORT", 587))
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = os.environ.get("EMAIL_USER")
-EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_PASSWORD")
+EMAIL_HOST_USER = os.environ.get("EMAIL_USER", "worldofcode777@gmail.com")
+EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_PASSWORD", "yopi gawa frtq noif")
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
 # =========================
