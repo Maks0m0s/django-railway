c743aded93866b456532598a7dff6eacb500f096
Maks0m0s <maksym.kozlovskyi150210@gmail.com>
2026-01-31 18:50:27 +0100

    email in base.py to none test

--- config/settings/base.py
+++ config/settings/base.py
@@ -152,7 +152,7 @@ EMAIL_PORT = int(os.environ.get("EMAIL_PORT", 587))
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = os.environ.get("EMAIL_USER")
 EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_PASSWORD")
-DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+DEFAULT_FROM_EMAIL = None
 
 # =========================
 # CELERY
