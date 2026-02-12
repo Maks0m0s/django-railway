aa379a3a635122d043d61e10b2e233fa0ae00d3d
Maks0m0s <maksym.kozlovskyi150210@gmail.com>
2026-02-12 14:26:05 +0100

    Email function fix

--- config/settings/base.py
+++ config/settings/base.py
@@ -162,13 +162,13 @@ REST_FRAMEWORK = {
 # EMAIL
 # =========================
 
-EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = int(os.environ.get("EMAIL_PORT", 587))
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = os.environ.get("EMAIL_USER")
 EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_PASSWORD")
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+EMAIL_BACKEND = "main.backends.email_backend.EmailBackend"
 
 # =========================
 # CELERY
