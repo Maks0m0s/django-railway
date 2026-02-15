76bea9384ce87f689aa3c31f0b9ccfc26246030d
Maks0m0s <maksym.kozlovskyi150210@gmail.com>
2026-02-15 13:35:13 +0100

    Settings Page Added

--- main/api/settings_views.py
+++ main/api/settings_views.py
@@ -7,16 +7,13 @@ from main.permissions import IsAuthenticatedOrRedirect
 from main.services import settings_service, feedback_service
 
 class SettingsViewSet(viewsets.ViewSet):
-    permission_classes = []
+    permission_classes = [IsAuthenticatedOrRedirect]
     renderer_classes = [TemplateHTMLRenderer]
+    template_name = "main/settings.html"
 
     def _get_context(self, user, settings=None):
-        if not settings:
+        if settings is None:
             settings = settings_service.get_settings(user)
-            if settings:
-                print('Profile Settings received !!!')
-            else:
-                print('Profile Settings wasnt received :(')
 
         return {
             "settings": settings,
@@ -25,16 +22,19 @@ class SettingsViewSet(viewsets.ViewSet):
         }
 
     def list(self, request):
-        return Response(self._get_context(request.user), template_name="main/settings.html")
+        return Response(
+            self._get_context(request.user),
+            template_name=self.template_name,
+        )
 
-    @action(detail=False, methods=["post"])
+    @action(detail=False, methods=["post"], url_path='update_settings')
     def update_settings(self, request):
         result = settings_service.update_settings(request.user, request.POST)
 
         if result["result"]:
             return Response(
                 self._get_context(request.user, result["settings"]),
-                template_name="main/settings.html"
+                template_name=self.template_name,
             )
 
         return Response(
--- main/services/settings_service.py
+++ main/services/settings_service.py
@@ -1,16 +1,22 @@
-from main.models import ProfileSettings
+from main.models import Settings
 
 def create_profile_settings(user):
     # just create the object, no need to save again
-    ProfileSettings.objects.create(user=user, is_public=True, hide_email=False)
+    try:
+        Settings.objects.create(user=user, is_public=True, hide_email=False)
+    except Exception as ex:
+        print(f"Couldn't create profile settings obj. Error : {ex}")
 
 def get_settings(user):
     # ensures settings always exist
-    settings, _ = ProfileSettings.objects.get_or_create(user=user)
-    return settings
+    try:
+        settings, _ = Settings.objects.get_or_create(user=user)
+        return settings
+    except Exception as ex:
+        print(f"Couldn't get profile settings obj. Error : {ex}")
 
 def update_settings(user, data):
-    settings, _ = ProfileSettings.objects.get_or_create(user=user)
+    settings, _ = Settings.objects.get_or_create(user=user)
 
     account_type = data.get('account_type', '')
     hide_email = data.get('hide_email') == 'on'
