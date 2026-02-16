726eafc6ade71fab46bd25545d8769a8a941786a
Maks0m0s <maksym.kozlovskyi150210@gmail.com>
2026-02-16 17:07:10 +0100

    Settings Page Updated

--- deploy_fix.py
+++
@@ -1,9 +0,0 @@
-import smtplib
-
-try:
-    server = smtplib.SMTP("smtp.gmail.com", 587, timeout=10)
-    server.starttls()
-    server.quit()
-    print("Connection successful!")
-except Exception as e:
-    print("Cannot connect:", e)
